20b5a81dd1bb12f2606b7e1a723313d8eff26059
minji <alswlchlrh8@naver.com>
2024-09-24 00:49:22 +0900

    [공통] 푸터 구현

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="display-flex-column gap-1r justify-center padding-70" style={{background: "rgb(34 47 63)"}}>
+                <li>
+                    <p className="color-white frank-800 font-size-24 text-align-center">DEVELOPER</p>
+                </li>
+                <p className="text-align-center weight-400 font-size-14 color-white" style={{color: "#bdbdbd"}}>
+                    &copy; {new Date().getFullYear()} DEVELOPER. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
