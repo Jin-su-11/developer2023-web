f9f01c86114033fc26a976b4e0443b9e0590822e
minji <alswlchlrh8@naver.com>
2024-09-27 00:26:47 +0900

    [공통] 푸터 개선

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,13 @@ import React from "react";
 const Footer = () => {
     return (
         <footer>
-            <div className="display-flex-column gap-1r justify-center padding-70" style={{background: "rgb(34 47 63)"}}>
+            <div className="display-flex-column gap-1r justify-center padding-70 width100"
+                 style={{background: "rgb(34 47 63)"}}>
                 <li>
                     <p className="color-white frank-800 font-size-24 text-align-center">DEVELOPER</p>
                 </li>
-                <p className="text-align-center weight-400 font-size-14 color-white" style={{color: "#bdbdbd"}}>
+                <p className="text-align-center weight-400 font-size-14 color-white"
+                   style={{color: "#bdbdbd"}}>
                     &copy; {new Date().getFullYear()} DEVELOPER. All rights reserved.</p>
             </div>
         </footer>
