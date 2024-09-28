2a2f446d02e9637e8067abc6a88ae81556eefccc
Jinsu <kimjs991229@gmail.com>
2024-09-28 15:52:57 +0900

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import '../css/style.css';
-import vectorRight from '../images/icon/vector_right_white.png';
+import '../css/style.css'
+import vectorRight from '../images/icon/vector_right_white.png'
 import { Link } from "react-scroll";
+import {useLocation} from "react-router-dom";
 
 const Header = ({ style = {background: "rgb(0 0 0 / 8%)"} }) => {
+    const location = useLocation();
+
     return (
-        <header className="padding-20" style={{ ...style }}>
+        <header className="padding-20" style={{...style}}>
             <nav>
                 <ul className="display-flex justify-between margin-0">
                     <li>
@@ -18,23 +21,27 @@ const Header = ({ style = {background: "rgb(0 0 0 / 8%)"} }) => {
                                 to="About"
                                 smooth={true}
                                 duration={500}
-                                className="color-white weight-400 hover-color-blue60"
+                                className="color-white weight-400 hover-color-blue60 hover-pointer"
                             >
                                 About
                             </Link>
                         </li>
                         <li>
-                            <a className="color-white weight-400 hover-color-blue60" href="/member">Member</a>
+                            <a className={`hover-color-blue60 
+                          ${location.pathname.startsWith('/member') ? 'color-blue weight-500' : 'color-white weight-400'}`}
+                               href="/member">Member</a>
                         </li>
                         <li>
-                            <a className="color-white weight-400 hover-color-blue60" href="/project">Project</a>
+                            <a className={`hover-color-blue60 
+                          ${location.pathname.startsWith('/project') ? 'color-blue weight-500' : 'color-white weight-400'}`}
+                               href="/project">Project</a>
                         </li>
                         <li>
                             <a className="color-white weight-400 hover-color-blue60" href="/">Contact</a>
                         </li>
                         <button className="custom-btn-primary color-white padding10-16 radius-8 weight-500 display-flex gap-5p hover-pointer hover-color-black10">
                             <p>가입하러 가기</p>
-                            <img src={vectorRight} className="width-12" alt="icon"/>
+                            <img src={vectorRight} className="width-12" />
                         </button>
                     </div>
                 </ul>
@@ -42,5 +49,7 @@ const Header = ({ style = {background: "rgb(0 0 0 / 8%)"} }) => {
         </header>
     );
 }
+
+
 
 export default Header;
