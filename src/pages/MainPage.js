b6b6564af1359ef049d3ca628163392cdcbcc93f
Jinsu <kimjs991229@gmail.com>
2024-10-28 20:08:24 +0900

    d

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,7 +6,6 @@ import Project from "../components/Project";
 import About from "../components/About";
 import Team from "../components/Team";
 import StackUpPage from "../components/StackUpPage";
-import ProjectPage from "./ProjectPage";
 import MainBackground from "../components/MainBackground";
 
 const MainPage = () => {
