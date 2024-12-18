2249f6fc9c1b6bd54b0ee2a0c39662f3fe5d1db9
minji <alswlchlrh8@naver.com>
2024-12-18 23:09:30 +0900

    fix: 컴포넌트 확인 방식을 type 일치 여부만 확인하도록 개선

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import About from "./About";
+import Team from "./Team";
+import Project from "./Project";
+import Progress from "./Progress";
 
 /**
  * About 컴포넌트
@@ -11,16 +15,16 @@ const MainContainer = ({ children }) => {
     return (
         <div className="main-container-outside">
             {React.Children.map(children, (child) => {
-                let backgroundColor = "#ffffff"; // 기본 배경색
+                let backgroundColor = "#000000"; // 기본 배경색
 
                 // 각 child의 타입에 따라 배경색을 변경
-                if (child.type && child.type.name === "About") {
+                if (child.type === About) {
                     backgroundColor = "#191919";
-                } else if (child.type && child.type.name === "Team") {
+                } else if (child.type === Team) {
                     backgroundColor = "#000000";
-                } else if (child.type && child.type.name === "Project") {
+                } else if (child.type === Project) {
                     backgroundColor = "#191919";
-                } else if (child.type && child.type.name === "Progress") {
+                } else if (child.type === Progress) {
                     backgroundColor = "#000000";
                 }
                 // 개별 child를 배경색을 가진 div로 감싸고, .main-container 스타일 유지
